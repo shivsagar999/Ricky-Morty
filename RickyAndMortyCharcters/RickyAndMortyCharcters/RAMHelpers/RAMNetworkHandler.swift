9d9f5cb30fb32f7e8fc64c52e56214551279ee7e
Shivsagar S Wagle <Shivsagarswagle@eurofins.com>
2022-11-04 08:17:07 +0530

    Added no network view controller

---
+++ RickyAndMortyCharcters/RickyAndMortyCharcters/RAMHelpers/RAMNetworkHandler.swift
@@ -0,0 +1,8 @@
+//
+//  RAMNetworkHandler.swift
+//  RickyAndMortyCharcters
+//
+//  Created by MEP LAB 01 on 03/11/22.
+//
+
+import Foundation
---
+++ RickyAndMortyCharcters/RickyAndMortyCharcters/RAMHelpers/RAMNetworkStatus.swift
@@ -0,0 +1,13 @@
+//
+//  RAMNetworkUtils.swift
+//  RickyAndMortyCharcters
+//
+//  Created by MEP LAB 01 on 02/11/22.
+//
+
+import Foundation
+
+class RAMNetworkUtils {
+    
+    
+}
